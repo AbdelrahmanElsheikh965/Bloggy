f430ac4b6a4cb61349eeaa6dd7d2ebab710fbfe6
Abdelrahman Elsheikh <58749776+AbdelrahmanElsheikh965@users.noreply.github.com>
2021-05-05 14:09:29 +0200

    deletingTheOldOnes

--- Bloggy/dbConfig.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$conn = mysqli_connect("localhost", "root", "", "blog");
-if(!$conn){
- die('Could not Connect My Sql:' .mysql_error());
-}
-
-?>
--- Bloggy/delete.php
+++
@@ -1,7 +0,0 @@
-<?php
-    require_once("dbConfig.php");
-    $id = $_GET['id'];
-    $query =  "DELETE FROM posts WHERE id = '$id'";
-    $result = mysqli_query($conn, $query);
-    header("Location: index-design.php");
-?>
